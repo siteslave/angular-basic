bba8c8d9e261f650629953c71273b820a82b12fe
Satit Rianpit <siteslave@msn.com>
2016-05-13 14:15:54 +0700

    init step 3

--- app.js
+++ app.js
@@ -2,7 +2,32 @@
 angular.module('app', [])
   .controller('MainCtrl', function ($scope, $log) {
 
-    $log.info('Welcome to angular application.');
-    $scope.message = 'Hello world';
+    $scope.users = [
+      { id: '1', name: 'Satit Rianpit' },
+      { id: '2', name: 'Steve Jobe'}
+    ];
+
+    $scope.add = function () {
+      var obj = {};
+      obj.id = Date.now();
+      obj.name = !$scope.newName ? 'Default name' : $scope.newName;
+      $scope.users.push(obj);
+      // clear text
+      $scope.newName = null;
+    };
+
+    $scope.edit = function (index) {
+      var user = $scope.users[index];
+      var newName = prompt('Enter new name', user.name);
+      if (newName) {
+        $scope.users[index].name = newName;
+      }
+    };
+
+    $scope.remove = function (index) {
+      if (confirm('Are you sure?')) {
+        $scope.users.splice(index, 1);
+      };
+    };
 
   });
