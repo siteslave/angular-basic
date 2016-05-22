560fcd3d30478d4520dbc8f7bfd568981cfd550d
Satit Rianpit <siteslave@msn.com>
2016-05-22 21:15:38 +0700

    success step7

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 // Basic application
-angular.module('app', [])
-  .controller('MainCtrl', function ($scope, $log) {
+angular.module('app', ['ui.router'])
 
-    $log.info('Welcome to angular application.');
-    $scope.message = 'Hello world';
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: './templates/main.html',
+        controller: function ($scope) {
+          $scope.message = 'Main page';
+        }
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: './templates/about.html',
+        controller: function ($scope) {
+          $scope.message = 'About Page';
+        }
+      })
+      .state('new', {
+        url: '/new',
+        templateUrl: './templates/new.html',
+        controller: function ($scope) {
+          $scope.message = 'New Page';
+        }
+      });
+
+  })
+  .controller('MainCtrl', function ($scope, $log) {
 
   });
